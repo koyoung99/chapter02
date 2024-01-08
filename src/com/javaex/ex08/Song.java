b8d0ad01a77d481d0e43b6330cf23af80b3c90ba
김아영 <72481734+koyoung99@users.noreply.github.com>
2024-01-08 14:48:13 +0900

    Add files via upload

---
+++ src/com/javaex/ex08/Song.java
@@ -0,0 +1,71 @@
+package com.javaex.ex08;
+
+public class Song {
+
+	private String title, artist, album, composer;
+	private int year, track;
+
+	public Song(String title, String artist, String album, String composer, int year, int track) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		this.track = track;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getComposer() {
+		return composer;
+	}
+
+	public void setComposer(String composer) {
+		this.composer = composer;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getTrack() {
+		return track;
+	}
+
+	public void setTrack(int track) {
+		this.track = track;
+	}
+
+	@Override
+	public String toString() {
+		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + composer + ", year="
+				+ year + ", track=" + track + "]";
+	}
+
+}
---
+++ src/com/javaex/ex08/SongApp.java
@@ -0,0 +1,13 @@
+package com.javaex.ex08;
+
+public class SongApp {
+
+	public static void main(String[] args) {
+
+		Song s01 = new Song("좋은날", "아이유", "아이쿠", "김민기", 2007, 2);
+
+		System.out.println(s01.toString());
+
+	}
+
+}
