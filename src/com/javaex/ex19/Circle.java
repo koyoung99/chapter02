3ae83747f219c37e11e8a277d0d1bbc49ccb3b80
김아영 <72481734+koyoung99@users.noreply.github.com>
2024-01-09 15:51:12 +0900

    20240109

---
+++ src/com/javaex/ex19/Circle.java
@@ -0,0 +1,39 @@
+package com.javaex.ex19;
+
+public class Circle extends Shape {
+
+	private int radius;
+
+	public Circle() {
+		super();
+	}
+
+	public Circle(int radius, String fillColor, String lineColor) {
+		super(fillColor, lineColor);
+		this.radius = radius;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", getFillColor()=" + getFillColor() + ", getLineColor()=" + getLineColor()
+				+ "]";
+	}
+
+	public void draw() {
+		// 원을 시각화 하는 복잡한 로직작동
+		System.out.println("======원을 그렸습니다=========");
+		System.out.println("반지름:" + radius);
+		System.out.println("면색:" + super.getFillColor());
+		System.out.println("선색:" + super.getLineColor());
+		System.out.println("============================");
+	}
+
+}
---
+++ src/com/javaex/ex19/Tringle.java
@@ -0,0 +1,49 @@
+package com.javaex.ex19;
+
+public class Tringle extends Shape {
+
+	private int width, height;
+
+	public Tringle() {
+		super();
+	}
+
+	public Tringle(int width, int height, String fillColor, String lineColor) {
+		super(fillColor, lineColor);
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "Tringle [width=" + width + ", height=" + height + ", getFillColor()=" + getFillColor()
+				+ ", getLineColor()=" + getLineColor() + "]";
+	}
+
+	public void draw() {
+		// 삼각형을 시각화 하는 복잡한 로직작동
+		System.out.println("====삼각형을 그렸습니다=========");
+		System.out.println("가로:" + width);
+		System.out.println("세로:" + height);
+		System.out.println("면색:" + super.getFillColor());
+		System.out.println("선색:" + super.getLineColor());
+		System.out.println("============================");
+	}
+
+}
